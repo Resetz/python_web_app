30b386a2092170188dee219ef9a3b9b2a8337e8a
Resanity <vincent.samuel.tran07@gmail.com>
2019-05-05 18:24:00 -0700

    Fixed Allowed Hosts

--- myWebsite/settings.py
+++ myWebsite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = ')&^@mpg+#!no%-+3^yik6a_4+^zp&w1s3c4j)1@=5a!q!dqbn0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://vast-hollows-53175.herokuapp.com/'
+]
 
 
 # Application definition
@@ -121,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
